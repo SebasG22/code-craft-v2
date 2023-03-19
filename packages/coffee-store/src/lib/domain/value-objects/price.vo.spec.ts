cf4acfb337e39f87dac61c4d0e3ff0b727c2c89f
Sebastián Guevara <rlxsebas@gmail.com>
2023-03-19 17:23:21 +0000

    feat(coffee-store): adding price.vo.spec

---
+++ packages/coffee-store/src/lib/domain/value-objects/price.vo.spec.ts
@@ -0,0 +1,17 @@
+import { Price } from "./price.vo";
+
+describe('Price VO', ()=>{
+    it('should create a price instance', ()=>{
+        const price = Price.create({
+            value: 0
+        });
+
+        expect(price.value).toEqual(0)
+    })
+
+    it('should throw an error when using an invalid price', ()=>{
+        expect(() => Price.create({
+            value: -1
+        })).toThrowError(/Price should be greather or equal to 0/)
+    })
+});
--- packages/coffee-store/src/lib/domain/value-objects/price.vo.ts
+++ packages/coffee-store/src/lib/domain/value-objects/price.vo.ts
@@ -9,7 +9,10 @@ export interface PriceProps {
 export class Price {
   constructor(private data: PriceValue) {}
   static create(data: PriceProps) {
-    return new Price(data);
+    if(data.value > -1) {
+      return new Price(data);
+    } 
+    throw new Error("Price should be greather or equal to 0");
   }
 
   get value(){
