429793734fa188cfad62d0df25a0834b9f3392f4
Sebastián Guevara <rlxsebas@gmail.com>
2023-03-19 16:35:31 -0500

    chore(repo): cover repo by ci (#2)

--- packages/coffee-store/src/lib/data/shoppingCartInMemory.ts
+++ packages/coffee-store/src/lib/data/shoppingCartInMemory.ts
@@ -1,24 +1,25 @@
-import { CoffeeEntity } from "../domain/entities/coffee.entity";
-import { ShoppingCartEntity } from "../domain/entities/shopping-cart.entity";
-import { ShoppingCartRepository } from "../domain/repositories/shopping-cart.repository";
+import { ShoppingCartEntity } from '../domain/entities/shopping-cart.entity';
+import { ShoppingCartRepository } from '../domain/repositories/shopping-cart.repository';
 
 export class ShoppingCartInMemory implements ShoppingCartRepository {
-    public cart = ShoppingCartEntity.create({items: []});
+  public cart = ShoppingCartEntity.create({ items: [] });
 
-    save(cart: ShoppingCartEntity){
-        this.cart = cart;
-        console.warn("save", JSON.stringify(cart))
-    }
+  save(cart: ShoppingCartEntity) {
+    this.cart = cart;
+    console.warn('save', JSON.stringify(cart));
+  }
 
-    get(): ShoppingCartEntity {
-        return this.cart;
-    }
+  get(): ShoppingCartEntity {
+    return this.cart;
+  }
 
-    getTotal(): number {
-        console.warn("aca" , JSON.stringify(this.cart));
-       return this.cart.value.items.reduce(
-            (accumulator, currentValue) => accumulator + currentValue.type.value.price.value * currentValue.quantity,
-            0
-          );
-    }
+  getTotal(): number {
+    console.warn('aca', JSON.stringify(this.cart));
+    return this.cart.value.items.reduce(
+      (accumulator, currentValue) =>
+        accumulator +
+        currentValue.type.value.price.value * currentValue.quantity,
+      0
+    );
+  }
 }
--- packages/coffee-store/src/lib/domain/use-cases/get-all-coffees.spec.ts
+++ packages/coffee-store/src/lib/domain/use-cases/get-all-coffees.spec.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { CoffeeEntity } from '../entities/coffee.entity';
 import { GetAllCoffeesUseCase } from './get-all-coffees';
 
-const coffess= [CoffeeEntity.create({
+const coffess = [
+  CoffeeEntity.create({
     ingredients: [],
     name: 'Cappuccino',
     price: 2,
@@ -12,23 +14,19 @@ const coffess= [CoffeeEntity.create({
     name: 'Mocha',
     price: 5,
     id: 'demo-mocha',
-  })
+  }),
 ];
-  
-  
-describe('Get All Coffees Use Case', ()=>{
 
-    it('Should return all coffess', ()=>{
-        const useCase = new GetAllCoffeesUseCase(
-            {
-                getAll: ()=> coffess,
-                 getCoffeeByName:(name: string) => {},
-                save:(coffee: CoffeeEntity) => {},
-            }
-        );
+describe('Get All Coffees Use Case', () => {
+  it('Should return all coffess', () => {
+    const useCase = new GetAllCoffeesUseCase({
+      getAll: () => coffess,
+      getCoffeeByName: () => {},
+      save: () => {},
+    });
 
-        const result = useCase.execute();
+    const result = useCase.execute();
 
-        expect(result).toEqual(coffess);
-    })
+    expect(result).toEqual(coffess);
+  });
 });
