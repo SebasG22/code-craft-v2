a16b76677efdb23aaf0333e415635e973c841096
Sebastian Guevara <info@sebasg.dev>
2023-03-24 08:09:29 -0500

    chore(repo): testing cloudflare

---
+++ packages/shopping-cart-qwik/functions/[[path]].ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line
+// @ts-ignore
+
+// Cloudflare Pages Functions
+// https://developers.cloudflare.com/pages/platform/functions/
+export { onRequest } from '../../../dist/packages/shopping-cart-qwik/server/entry.cloudflare-pages';
