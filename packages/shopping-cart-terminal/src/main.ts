8c39ca39bd1657ea7015a97208e26809204fe7fa
Sebastian Guevara <info@sebasg.dev>
2023-03-27 05:49:17 -0500

    feat(shopping-cart-qwik): adding pay preview flow.spec

--- packages/shopping-cart-terminal/src/main.ts
+++ packages/shopping-cart-terminal/src/main.ts
@@ -1,8 +1,6 @@
-import { CoffeeTerminalView } from "./presentation/coffee.view";
-import { getCoffeePresenter } from "./presentation/dependenciesLocator";
+import { CoffeeTerminalView } from './presentation/coffee.view';
+import { getCoffeePresenter } from './presentation/dependenciesLocator';
 
-console.log('Hello World');
 const view = new CoffeeTerminalView();
 const presenter = getCoffeePresenter(view);
 presenter.start();
-
