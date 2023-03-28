e14d96a6309bb4d0a50b9af1529ce29a34f5ebf5
Sebastian Guevara <info@sebasg.dev>
2023-03-28 18:08:54 -0500

    chore(shopping-cart-qwik): change e2e test port

--- packages/shopping-cart-qwik-e2e/playwright.config.ts
+++ packages/shopping-cart-qwik-e2e/playwright.config.ts
@@ -4,14 +4,14 @@ const config: PlaywrightTestConfig = {
   outputDir: 'dist',
   webServer: {
     cwd: '../../',
-    command: 'nx run shopping-cart-qwik:serve',
-    port: 4200,
+    command: 'nx run shopping-cart-qwik:serve --port 5000',
+    port: 5000,
     timeout: 50000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
     trace: 'on',
-    baseURL: 'http://localhost:4200/',
+    baseURL: 'http://localhost:5000/',
   },
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
