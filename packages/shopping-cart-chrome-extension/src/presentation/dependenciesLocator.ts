c134655e470f585991f11106bf8782416da92632
Sebastian Guevara <info@sebasg.dev>
2023-03-29 21:22:12 -0500

    feat(shopping-cart-chrome-extension): adding motion animations to coffee list

--- packages/shopping-cart-chrome-extension/src/presentation/dependenciesLocator.ts
+++ packages/shopping-cart-chrome-extension/src/presentation/dependenciesLocator.ts
@@ -7,11 +7,25 @@ import {
   RemoveOneItemFromShoppingCartUseCase,
   SaveItemShoppingCartUseCase,
   ShoppingCartInMemory,
+  ShoppingCartEntity,
+  data_coffees,
+  CoffeeEntity,
 } from '@code-craft/coffee-store';
 
 export const getCoffeeControllerVue = () => {
-  const coffeeInMemory = new CoffeeInMemory();
-  const shoppingInMemory = new ShoppingCartInMemory();
+  const coffees = data_coffees
+    .filter((item) => item.availability)
+    .map((item) =>
+      CoffeeEntity.create({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        ingredients: item.recipe,
+      })
+    );
+  const cart = ShoppingCartEntity.create({ items: [] });
+  const coffeeInMemory = new CoffeeInMemory(coffees);
+  const shoppingInMemory = new ShoppingCartInMemory(cart);
 
   const getAllCoffeeUseCase = new GetAllCoffeesUseCase(coffeeInMemory);
   const getShoppingUseCase = new GetShoppingUseCase(shoppingInMemory);
